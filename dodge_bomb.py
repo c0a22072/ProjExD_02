baa77bf9b3be24d3c2ef8d80e89eeeabe765d4a6
SakamotoSoya <c0a22072df@edu.teu.ac.jp>
2023-06-20 16:38:17 +0900

    追加機能1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import random
+import math
 import sys
 import pygame as pg
 
@@ -9,7 +10,9 @@ delta = {
     pg.K_DOWN:(0, +5),
     pg.K_LEFT:(-5, 0),
     pg.K_RIGHT:(+5, 0)
-
+}
+keys_delta = {
+    
 }
 def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
     """
@@ -30,6 +33,18 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_2 = pg.transform.flip(kk_img, True, False)
+    kk_imgs = {
+        (+5, 0): kk_img_2,  # 右方向こうかとんの画像
+        (+5, -5): pg.transform.rotozoom(kk_img_2, 45, 1.0),  # 右上方向こうかとんの画像
+        (0, -5): pg.transform.rotozoom(kk_img_2, 90, 1.0),  # 上方向こうかとんの画像
+        (-5, -5): pg.transform.rotozoom(kk_img, -45, 1.0),  # 左上方向こうかとんの画像
+        (-5, 0): kk_img,  # 左方向こうかとんの画像
+        (-5, +5): pg.transform.rotozoom(kk_img, 45, 1.0),  # 左下方向こうかとんの画像
+        (0, +5): pg.transform.rotozoom(kk_img_2, -90, 1.0),  # 下方向こうかとんの画像
+        (+5, +5): pg.transform.rotozoom(kk_img_2, -45, 1.0),  # 右下方向こうかとんの画像
+        }
+    kk_img = kk_imgs[+5, 0]
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bd_img= pg.Surface((20,20))#練習1
@@ -40,8 +55,6 @@ def main():
     bd_rct = bd_img.get_rect()
     bd_rct.center = x,y
     vx, vy = +5, +5
-
-     
 
     clock = pg.time.Clock()
     tmr = 0
@@ -58,9 +71,24 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        key_lst = pg.key.get_pressed()
+        # こうかとんの画像方向を選ぶための変数
+        kk_0 = 0
+        kk_1 = 0
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+                kk_0 = kk_0 + mv[0]
+                kk_1 = kk_1 + mv[1]
+        
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
+        if kk_0 != 0 or kk_1 != 0:  # 飛ぶ方向に従ってこうかとん画像を切り替える
+            kk_img = kk_imgs[kk_0, kk_1]
+        screen.blit(kk_img, kk_rct)
+
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         screen.blit(bd_img, bd_rct)
